b50cd752b68a43f61ce5648331a3bca1fccdc9db
Alberth G. Ruado <alberthruado@gmail.com>
2023-03-02 12:44:04 +0800

    Done navigation functionality

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,6 @@
-console.log("script");
+const navToggle = document.querySelector(".icon-hamburger");
+const navList = document.querySelector(".nav-list");
+
+navToggle.addEventListener("click", function () {
+  navList.classList.toggle("active");
+});
